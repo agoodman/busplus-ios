9ef5c030ea9c53021ffdf79037e239b6d683d7bc
Aubrey <aubrey.goodman@gmail.com>
2012-11-23 23:17:32 -0500

    Initial Commit

---
+++ BusPlus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BusPlus
+//
+//  Created by Aubrey Goodman on 11/23/12.
+//  Copyright (c) 2012 Migrant Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BusPlus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BusPlus
+//
+//  Created by Aubrey Goodman on 11/23/12.
+//  Copyright (c) 2012 Migrant Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
